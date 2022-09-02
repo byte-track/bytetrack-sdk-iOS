0b95615a37699a34a5d773eb17c3f0748eb35166
“sunliang” <pro_great@126.com>
2022-09-02 16:14:33 +0800

    上传swiftDemo

---
+++ bytetrackDemo_Swift/bytetrackDemo_Swift/swiftCode-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  swiftCode-Bridging-Header.h
+//  bytetrackDemo_Swift
+//
+//  Created by 孙良 on 2022/9/2.
+//
+
+#ifndef swiftCode_Bridging_Header_h
+#define swiftCode_Bridging_Header_h
+#import <Bytetrack/Bytetrack.h>
+
+#endif /* swiftCode_Bridging_Header_h */
